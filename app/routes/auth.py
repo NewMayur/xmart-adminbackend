4e488c09357fed8e2b38958ae7dfcf17120e2528
MayurGharat <mayur.gharat@pragmatyc.com>
2024-07-08 11:57:19 +0530

    Added registration to Auth.py

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -4,6 +4,7 @@ from app.schema.User import User
 from app.schema.Property import MasterPropertyType, Property
 from app.extensions.db import db
 from app.extensions.responses import response_base
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
 
 
 @app.route("/_health", methods=["GET"])
@@ -19,18 +20,35 @@ def register007():
     user = User(username=request.json["username"], password=pwd_hash)
     db.session.add(user)
     db.session.commit()
-    return response_base(message="Success", status=200)
+    return response_base(message="Success", status=200,data=[])
 
 
 @app.route("/login", methods=["POST"])
 def all():
     # db.create_all()
-    data = request.json
-    students = User.query.filter_by(username=request.json["username"]).first()
-    print(students)
-    if (students is not None) and bcrypt.check_password_hash(
-        students.password, request.json["password"]
+    student = User.query.filter_by(username=request.json["username"]).first()
+    print(student)
+    if (student is not None) and bcrypt.check_password_hash(
+        student.password, request.json["password"]
     ):
-        return response_base(message="Success", status=200)
+        access_token = create_access_token(identity={'username': student.username})
+        return response_base(message="Authenticated", status=200, data=[{"token" : access_token}])
     else:
-        return response_base(message="Failed", status=404)
+        return response_base(message="Incorrect credentials", status=403, data=[])
+
+@app.route("/admin-mode", methods=["POST"])
+def admin_mode():
+    student = User.query.filter_by(username=request.json["username"]).first()
+    print(student)
+    if (student is not None) and bcrypt.check_password_hash(
+        student.password, request.json["password"]
+    ):
+        return response_base(message="Authenticated", status=200, data=[])
+    else:
+        return response_base(message="Incorrect credentials", status=403, data=[])
+
+@app.route("/test-auth", methods=["POST"])
+@jwt_required()
+def test_auth():
+    current_user = get_jwt_identity()
+    return current_user
