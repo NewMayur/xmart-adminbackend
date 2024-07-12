5cf68dd692b3500f421d6be1c110a6ad1eb8807e
MayurGharat <mayur.gharat@pragmatyc.com>
2024-07-12 15:50:58 +0530

    Completed input validations for property

--- app/schema/property_models.py
+++ app/schema/property_models.py
@@ -1,77 +1,74 @@
-from pydantic import BaseModel, EmailStr, Field, field_validator, model_validator
+from pydantic import BaseModel, EmailStr, Field, field_validator
 from typing import Optional
 
 class PropertyCreateModel(BaseModel):
     name: str 
     property_master_id: int
-    country: str
-    state: str
-    city: str
+    country: Optional[str] = ""
+    state: Optional[str] = ""
+    city: Optional[str] = ""
     address1: str
     address2: Optional[str] = ""
     banner_base64: str
     logo_base64: str
-    zip_code: str
-    primary_contact_name: str
+    zip_code: Optional[str] = Field("", max_length=5)
+    primary_contact_name: Optional[str] = Field("", max_length=50)
     primary_contact_email: EmailStr
     primary_contact_contact_number: str
-    primary_contact_job_title: str
-    primary_contact_phone_number_code: str
-
-    # @field_validator('name', 'address1', 'address2')
-    # def valid_alphanumeric(cls, values):
-    #     for field_name, value in values.items():
-    #         if not value.isalnum():
-    #             raise ValueError(f"{field_name} must be alphanumeric")
-    #     return values
+    primary_contact_job_title: Optional[str] = Field("", max_length=50)
+    primary_contact_phone_number_code: Optional[str] = Field("", max_length=3)
     
-    # @field_validator('name', 'address1', 'address2')
-    # def valid_alphanumeric(cls, v):
-    #     if not v.isalnum():
-    #         raise ValueError(f"name must be alphanumeric")
+    # @field_validator("name","address1")
+    # @classmethod
+    # def valid_alphanumeric(cls, v: str, info: ValidationInfo):
+    #     # if info.field_name == 'name':
+    #     #     if not name_pattern.match(v): raise ValueError(f'{v} is not a valid name.')
+    #     print(v)
+    #     if (info.field_name == 'name') and (not v.isalnum()):
+    #         raise ValueError(f"{v} must be alphanumeric")
+    #     if (info.field_name == 'address1') and (not v.isalnum()):
+    #         raise ValueError(f"{v} must be alphanumeric")
     #     return v
 
-
-    
-    @model_validator(mode='before')
-    def valid_alphanumerics(cls, values):
-        for field_name in ['name', 'address1', 'address2']:
-            
-            value = values.get(field_name)
-            print(value)
-            if value and not value.isalnum():
-                raise ValueError(f"{field_name} must be alphanumeric")
-        return values
-
-
+    @field_validator('name', 'address1', 'address2')
+    def valid_alphanum(cls, v, info):
+        if not v.isalnum():
+            raise ValueError(f"{info.field_name} must be alphanumeric")
+        return v
     
     @field_validator('primary_contact_contact_number')
     def validate_contact_number(cls, v):
         if len(v) != 10 or not v.isdigit():
-            raise ValueError('Contact number must be a 10-digit')
+            raise ValueError("Please enter a valid 10-digit phone number.")
         return v
 
 
 class PropertyUpdateModel(BaseModel):
     property_id: int
-    name: str
+    name: str 
     property_master_id: int
-    country: str
-    state: str
-    city: str
+    country: Optional[str] = ""
+    state: Optional[str] = ""
+    city: Optional[str] = ""
     address1: str
-    address2: str
+    address2: Optional[str] = ""
     banner_base64: str
     logo_base64: str
-    zip_code: str
-    primary_contact_name: str
+    zip_code: Optional[str] = Field("", max_length=5)
+    primary_contact_name: Optional[str] = Field("", max_length=50)
     primary_contact_email: EmailStr
     primary_contact_contact_number: str
-    primary_contact_job_title: str
-    primary_contact_contact_number_code: str
+    primary_contact_job_title: Optional[str] = Field("", max_length=50)
+    primary_contact_contact_number_code: Optional[str] = Field("", max_length=3)
 
+    @field_validator('name', 'address1', 'address2')
+    def valid_alphanum(cls, v, info):
+        if not v.isalnum():
+            raise ValueError(f"{info.field_name} must be alphanumeric")
+        return v
+    
     @field_validator('primary_contact_contact_number')
     def validate_contact_number(cls, v):
         if len(v) != 10 or not v.isdigit():
-            raise ValueError('Contact number must be a 10-digit')
+            raise ValueError("Please enter a valid 10-digit phone number.")
         return v
